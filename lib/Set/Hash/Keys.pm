70be2e659ecc98cd5216cfdf707b002b241e676b
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2018-07-15 14:32:31 +0200

    pass tests for `difference`

--- lib/Set/Hash/Keys.pm
+++ lib/Set/Hash/Keys.pm
@@ -9,4 +9,31 @@ sub new {
     return bless \%data, $class
 }
 
+sub difference {
+    return unless defined $_[0];
+    
+    if ( wantarray() ) {
+        my $sets_ref = [];
+        for my $i ( 0 .. $#_ ) {
+            my @other = @_; # make a clone, since splice mutates it
+            my $set_i = splice( @other, $i, 1 );
+            my $set_d = difference( $set_i, @other );   
+            push @$sets_ref, $set_d;
+        }
+        return @$sets_ref
+    }
+    
+    my $hash_ref = reduce {
+        +{
+            map {
+                $_, $a->{$_}
+            } grep {
+                !exists $b->{$_}
+            } keys %$a
+        }
+    } @_;
+    
+    __PACKAGE__->new( %$hash_ref )
+}
+
 1;
